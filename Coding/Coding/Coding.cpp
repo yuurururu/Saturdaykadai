4a1916d04c3e317fac7bdf265ae4a782b3d3f9e8
yuurururu <kujingsan@gmail.com>
2026-07-25 16:23:18 +0900

    ムーブとコピー

---
+++ Coding/Coding/Coding.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+// 整数型のデータを扱うクラス
+class MyClass {
+public:
+    // コンストラクタ
+    MyClass() = default;
+
+    // 引数付きコンストラクタ
+    MyClass(int size) {
+        data_ = new int[size];
+        dataSize_ = size;
+    }
+
+    // コピーコンストラクタ
+    MyClass(const MyClass& src) {
+
+        if (data_) {
+            delete[] data_;
+        }
+
+        // インスタンスを別に再生成
+        data_ = new int[src.dataSize_];
+        dataSize_ = src.dataSize_;
+
+        // 内容を全部設定する
+        for (auto i = 0; i < dataSize_; ++i) {
+            data_[i] = src.data_[i];
+        }
+
+        std::cout << "コピーコンストラクタ" << std::endl;
+    }
+
+    // ムーブコンストラクタ
+    MyClass(MyClass&& src) noexcept {
+
+        if (data_) {
+            delete[] data_;
+        }
+
+        // 再生成ではなく、src のdata_が指すアドレスをそのまま取得する
+        data_ = src.data_;
+        dataSize_ = src.dataSize_;
+
+        // src が破棄された際にデストラクタで delete されると問題なので初期化しておく
+        src.data_ = {};
+        src.dataSize_ = {};
+
+        std::cout << "ムーブコンストラクタ" << std::endl;
+    }
+
+
+    // デストラクタ
+    ~MyClass() {
+        if (data_) {
+            delete[] data_;
+        }
+        data_ = {};
+    }
+
+private:
+    int* data_ = {}; // データポインタ
+    int     dataSize_ = {}; // データサイズ
+};
+// templateもautoと同じ働きをする
+template<class T>
+void f(T&& a)
+{
+
+}
+
+// エントリー関数
+int main()
+{
+
+    MyClass src(1000);
+
+    // std::move(src) = 右辺値参照 rRef = 右辺値参照
+    auto&& rRef = std::move(src);
+    // src は左辺値 raf = 左辺値参照
+    auto&& rRef = src;
+    f(std::move(src));
+    f(src);
+    // 以降 src は data_ が nullptr になっている為利用できない
+    {
+        // ループ処理等
+    }
+
+    return 0;
+}
